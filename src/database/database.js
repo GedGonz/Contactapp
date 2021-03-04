42f8f00e0a570ec78c7b048571a4ee02fb9510a0
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-03 22:00:16 -0600

    config enviroment

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,8 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import config from '../config/configapp'
 
 (async() => {
-
-    const db = await mongoose.connect('mongodb://localhost/Contactdb', {
+    const db = await mongoose.connect(config.mongodbURL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
