3e5993a8ebe98ac5ad5a230132eabbd9d0e0586f
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-04 09:14:56 -0600

    configuring cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import IndexRouter from './routes/contact.routes';
-
+import cors from 'cors'
 const app = express();
 
 //settings
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.set('port', PORT);
 
+//middlewaras
+app.use(cors);
 app.use(express.json());
 
 //routes
