527be6fbfb72b63e299e3680b88665b9eab586f7
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-10 22:01:18 -0600

    new controller user

---
+++ src/controller/user.controller.js
@@ -0,0 +1,30 @@
+import Users from "../models/users";
+
+export const createNewUser = async(req, res) => {
+    try {
+
+        var newUser = new Users({ Username: req.body.Username, Password: req.body.Password });
+
+        var user = newUser.save();
+
+        res.json(user);
+
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+};
+
+export const getAllUsers = async(req, res) => {
+    try {
+
+        var users = Users.find();
+
+        if (!users)
+            return res
+                .status(400)
+                .json({ message: 'not found users' });
+
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+};
